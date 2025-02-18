19a501e3ffe3ef3829ed1ba1eff06c824b270fab
CamillaEva <caha0007@stud.kea.dk>
2025-02-18 10:13:04 +0100

    hello

---
+++ src/main/java/com/example/pullrequestoevelse/PullRequestOevelseApplication.java
@@ -0,0 +1,13 @@
+package com.example.pullrequestoevelse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PullRequestOevelseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PullRequestOevelseApplication.class, args);
+    }
+
+}
